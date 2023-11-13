3640481ec0ddc580715ae28d2f583948500a3c86
ryoohtani <kazuinto64@gmail.com>
2023-11-13 22:33:18 +0900

    form入力項目

--- app/frontend/react-my-app/src/Form.js
+++ app/frontend/react-my-app/src/Form.js
@@ -1,3 +1,27 @@
-export const Form = () => {
-    return <div>フォームです</div>
+import { useState } from "react";
+
+export const Form = ( {onAddLang} ) => {
+    //textステートをuseStateで定義
+    const [text, setText] = useState('');
+
+    const submitForm = (event) => {
+        // preventDefaultでページ遷移を発生しないようにする
+        event.preventDefault();
+        onAddLang(text);
+    }
+    
+    return (
+    <div>
+        <h4>新しい言語の追加</h4>
+        <form onSubmit={submitForm}>
+            <div>
+                {/* 定義したtextをinputのvalueで設定、onChangeのsetTextで用いてtextに反映する */}
+                <input type = "text" value = {text} onChange = {(e) => setText(e.target.value)}/>
+            </div>
+            <div>
+                <button>追加</button>
+            </div>
+        </form>
+    </div>
+    )
 }
