859ca9b9eb96907c02c7c401c3905f0d21972f8b
ryoohtani <kazuinto64@gmail.com>
2023-11-13 22:33:50 +0900

    リストに記載するシステム

--- app/frontend/react-my-app/src/List.js
+++ app/frontend/react-my-app/src/List.js
@@ -1,8 +1,12 @@
-export const List = ({title}) => {
+export const List = ({langs}) => {
     return (
         <div>
-            <h4>{title}</h4>
-            <div>リスト</div>
+
+            {
+                langs.map((lang, index) => {
+                    return <div key={index}> { lang }</div>
+                })
+            }
         </div>
-    );
+    )
 }
